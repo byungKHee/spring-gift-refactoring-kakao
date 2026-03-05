bf77dedb2185c6cb5b4602fef65511df246f13e4
byungKHee <byungheekang@g.skku.edu>
2026-03-05 12:23:15 +0900

    refactor(entity): @ManyToOne fetch 전략 LAZY 명시

--- src/main/java/gift/option/Option.java
+++ src/main/java/gift/option/Option.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -17,7 +18,7 @@ public class Option {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
